965a366bffffbef46162ff043f67cace51366eb9
Karl Lew <karl@firepick.org>
2018-08-07 13:25:31 -0700

    start

---
+++ scripts/init.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+if [ -e node_modules ]; then
+    echo -e "INIT\t: node_modules exist. no action required"
+else
+    echo -e "INIT\t: npm install..."
+    npm install
+fi
+
+if [ -e local/mn ]; then
+    echo -e "INIT\t: mn folder exists. no action required"
+    exit 0
+fi
+
+mkdir -p local
+pushd local
+
+echo -e "INIT\t: loading SuttaCentral translation content (translation.zip)..."
+curl https://codeload.github.com/suttacentral/translation/zip/master -o master.zip
+unzip master.zip
+mv translation-master/mn .
+rm -rf master.zip translation-master
+
+popd
---
+++ test/js-pootl.js
@@ -0,0 +1,9 @@
+
+(typeof describe === 'function') && describe("js-pootl", function() {
+    const should = require("should");
+
+    it("TESTTESThello", function() {
+        should(1).equal(1);
+    });
+
+})
