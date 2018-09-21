7631be3bef5ca98d7258ddebe3c966f909e97283
Karl Lew <karl@firepick.org>
2018-09-21 08:55:33 -0700

    throttle

--- src/node/abstract-tts.js
+++ src/node/abstract-tts.js
@@ -21,7 +21,7 @@
             this.api = opts.api || null;
             this.apiVersion = opts.apiVersion || null;
             this.audioSuffix = opts.audioSuffix || ".ogg";
-            this.queue = new Queue(opts.maxConcurrentServiceCalls || 20, Infinity);
+            this.queue = new Queue(opts.maxConcurrentServiceCalls || 10, Infinity);
             this.usage = opts.usage || "recite";
             this.usages = opts.usages || {};
             this.mj = new MerkleJson({
