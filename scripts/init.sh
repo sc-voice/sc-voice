b652af805576c3e3558933c8cf71bbd04c0b610c
Karl Lew <karl@firepick.org>
2019-03-29 13:47:41 -0700

    init

--- scripts/init.sh
+++ scripts/init.sh
@@ -20,7 +20,7 @@ RC=$?; if [ "$RC" == "0" ]; then
     echo -e "INIT\t: build-essentials detected (OK)"
 else
     echo -e "INIT\t: installing build-essentials (requires sudo)"
-    sudo apt-get install -y build-essential
+    apt-get install -y build-essential
 fi
 
 type npm
