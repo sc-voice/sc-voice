2c678ff9722bbee2a1fa05c65e62842c41224a54
Karl Lew <karl@firepick.org>
2019-07-03 13:04:59 -0700

    logs

--- scripts/sc-voice-start.sh
+++ scripts/sc-voice-start.sh
@@ -5,11 +5,11 @@ echo $SCRIPT_DIR
 APP_DIR=`realpath $SCRIPT_DIR/..`
 LOGDIR="${APP_DIR}/local/logs"
 mkdir -p ${LOGDIR}
-LOGFILE="${LOGDIR}/scv-%Y%m%d.%H%M.log"
+LOGFILE="${LOGDIR}/scv-%Y%m%d.log"
 SCVLOG="${APP_DIR}/local/scv.log" 
 rm -f $SCVLOG
 
-${SCRIPT_DIR}/sc-voice.js --ssl 2>&1 | rotatelogs -L ${APP_DIR} -f ${LOGFILE} 86400
+${SCRIPT_DIR}/sc-voice.js --ssl 2>&1 | rotatelogs -L ${SCVLOG} -f ${LOGFILE} 86400
 
 echo -e "`date` EXIT sc-voice" | tee -a ${SCVLOG}
 
